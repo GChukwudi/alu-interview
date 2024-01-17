16c9be16648cd38992a960f9f6ccb5105c9de886
GChukwudi <g.chukwudi@alustudent.com>
2024-01-17 14:39:55 +0000

    updated

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,6 +3,7 @@
 This script defines a function 'minOperation' to calculate the fewest number
 of operations needed to obtain 'n' 'H' characters in the file.
 """
+import math
 
 
 def minOperation(n):
@@ -17,10 +18,14 @@ def minOperation(n):
   """
   if n <= 1:
     return 0
+  
+  operations = 0
+  for i in range(2, int(math.sqrt(abs(n))) + 1):
+    while n % i == 0:
+        operations += i
+        n //= i
 
-  if n % 2 == 0:
-    return 1 + minOperation(n / 2)
-  return 1 + minOperation(n - 1)
+  if n > 1:
+    operations += n
 
-n = 4
-print(minOperation(n))
+  return operations
