26a019662b061b593c7a657fe6eeee78a4f948af
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-02-24 10:20:47 +0200

    updated

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -6,19 +6,11 @@ def pascal_triangle(n):
     """Returns a list of lists of integers"""
     if n <= 0:
         return []
-    pascal = []
-    for i in range(n):
-        if i == 0:
-            pascal.append([1])
-        elif i == 1:
-            pascal.append([1, 1])
-        else:
-            row = [1]
-            for j in range(1, i):
-                row.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
-            row.append(1)
-            pascal.append(row)
-    return pascal
-
-n = 5
-print(pascal_triangle(n))
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
